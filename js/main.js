f2096d01fa3a5faaeed6f0ac0e8a11ca4357b0fa
arsen <baregamyan@gmail.com>
2020-07-28 02:53:13 +0300

    Почти доделывает валидацию (остается только REGEXP)

--- js/main.js
+++ js/main.js
@@ -76,14 +76,45 @@ var Config = {
   },
   VALIDITY: {
     HASHTAG: {
-      LENGTH: {MAX: 20, getMessage: function() {return 'Хештег привышает максимальную длину в ' + this.MAX + ' символов. Проверьте хештег: '}},
-      QUANTITY: {MAX: 5, getMessage: function() {return 'Максимальное количество хештегов ' + this.MAX + ' символов. Проверьте хештег: '}},
-      FIRST_SYMBOL: {SYMBOL: '#', getMessage: function() {return 'Первым символом в хештеге должен быть «' + this.SYMBOL + '». Проверьте хештег: '}},
-      ALLOWED_SYMBOLS: {REGEXP: /^[0-9a-zA-Z]+$/, getMessage: function() {return 'Хештег должен содержать только буквы и цифры. Проверьте хештег: '}},
-      UNIQUE: {getMessage: function() {return 'Хештеги не должны повторяться. Повтор хештэга: '}}
+      LENGTH: {
+        MAX: 20,
+        getMessage: function () {
+          return 'Хештег привышает максимальную длину в ' + this.MAX + ' символов. Проверьте хештег: ';
+        }
+      },
+      QUANTITY: {
+        MAX: 5,
+        getMessage: function () {
+          return 'Максимальное количество хештегов ' + this.MAX + ' символов. Проверьте хештег: ';
+        }
+      },
+      FIRST_SYMBOL: {
+        SYMBOL: '#',
+        getMessage: function () {
+          return 'Первым символом в хештеге должен быть «' + this.SYMBOL + '». Проверьте хештег: ';
+        }
+      },
+      ALLOWED_SYMBOLS: {
+        REGEXP: /^[0-9a-zA-Z]+$/,
+        getMessage: function (isEmpty) {
+          if (isEmpty) {
+            return 'Хештег не может содержать только «' + this.FIRST_SYMBOL_EXEPTION + '»';
+          } else {
+            return 'Хештег должен содержать только буквы и цифры. Проверьте хештег: ';
+          }
+        },
+        FIRST_SYMBOL_EXEPTION: '#'
+      },
+      UNIQUE: {
+        getMessage: function () {
+          return 'Хештеги не должны повторяться. Повтор хештэга: ';
+        }
+      }
     },
     COMMENT: {
-      LENGTH: {MAX: 140, getMessage: function() {return 'Комментарий привышает максимальную длину в' + this.MAX + 'символов.'}}
+      LENGTH: {MAX: 140, getMessage: function () {
+        return 'Комментарий привышает максимальную длину в' + this.MAX + 'символов.';
+      }}
     }
   }
 };
@@ -569,19 +600,29 @@ function Validation(form, config) {
   this.input = {
     hashtag: {
       element: this.form.querySelector('.text__hashtags'),
-      getValue: function() {return getInputValue(this.element).trim()},
+      getValue: function () {
+        return getInputValue(this.element).trim();
+      },
       config: this.config.HASHTAG,
       errors: [],
-      getErrors: function() {return this.errors.join('. \n')}
+      getErrors: function () {
+        return this.errors.join('.     \n');
+      }
     },
     comment: {
       element: this.form.querySelector('.text__description'),
-      getValue: function() {return getInputValue(this.element).trim()},
+      getValue: function () {
+        return getInputValue(this.element).trim();
+      },
       config: this.config.COMMENT,
       errors: [],
-      getErrors: function() {return this.errors.join('. \n')}
+      getErrors: function () {
+        return this.errors.join('. \n');
+      }
     }
   };
+  this.invalidities = [];
+
   this.isValid = true;
 
   this.onInputFocus = this.focus.bind(this);
@@ -611,87 +652,125 @@ Validation.prototype.addInvalidity = function (message) {
   this.invalidities.push(message);
 };
 
-Validation.prototype.invalidities = function () {
+Validation.prototype.getInvalidities = function () {
   return this.invalidities.join('. \n');
 };
 
 Validation.prototype.check = function () {
   this.invalidities = [];
-  this.input.hashtag.errors = [];
-  this.input.comment.errors = [];
-
-  var hashtags = this.input.hashtag.getValue().split(' ');
-
-  this.checkLength(this.input.comment.errors, this.input.comment.getValue(), this.input.comment.config.LENGTH);
-  this.checkQuantity(this.input.hashtag.errors, this.input.hashtag.getValue(), this.input.hashtag.config.QUANTITY);
-  this.isUnique(this.input.hashtag.errors, hashtags, this.input.hashtag.config.UNIQUE);
-
-  for (var i = 0; i < hashtags.length; i++) {
-    this.checkLength(this.input.hashtag.errors, hashtags[i], this.input.hashtag.config.LENGTH);
-    this.isFirstSymbol(this.input.hashtag.errors, hashtags[i], this.input.hashtag.config.FIRST_SYMBOL);
-    this.checkSymbols(this.input.hashtag.errors, hashtags[i], this.input.hashtag.config.ALLOWED_SYMBOLS);
-  }
-  if (!this.input.hashtag.errors.length && !this.input.comment.errors.length) {
-    this.isValid = true;
+  if (this.input.hashtag.getValue() !== '') {
+    this.checkHashtags(this.input.hashtag);
   } else {
-    this.showErrors(Object.values(this.input));
+    this.input.hashtag.errors = [];
+  }
+  if (this.input.comment.getValue() !== '') {
+    this.checkComment(this.input.comment);
+  } else {
+    this.input.comment.errors = [];
+  }
+  this.showErrors(Object.values(this.input));
+};
+
+Validation.prototype.checkHashtags = function (input) {
+  input.errors = [];
+  this.checkQuantity(input);
+  this.checkLength(input, true);
+  this.isFirstSymbol(input);
+  this.isUnique(input);
+  this.checkSymbols(input);
+};
+
+Validation.prototype.checkComment = function (input) {
+  input.errors = [];
+  this.checkLength(input, false);
+};
+
+Validation.prototype.checkLength = function (input, perWord) {
+  var _config = input.config.LENGTH;
+  var _max = _config.MAX;
+  var _value = input.getValue();
+  var error;
+  if (perWord) {
+    var hashtags = _value.split(' ');
+    for (var i = 0; i < hashtags.length; i++) {
+      var hashtag = hashtags[i];
+      if (hashtag.length > _max) {
+        error = _config.getMessage() + ' «' + hashtag + '»';
+        input.errors.push(error);
+      }
+    }
+  } else {
+    if (_value.length > _max) {
+      error = _config.getMessage() + ' «' + _value + '»';
+      input.errors.push(error);
+    }
   }
 };
 
-Validation.prototype.checkLength = function (errors, value, config) {
-  var _max = config.MAX;
-  if (value > _max) {
-    var error = config.getMessage() + ' «' + value + '»';
-    this.invalidities.push(error);
-    errors.push(error);
+Validation.prototype.isFirstSymbol = function (input) {
+  var _config = input.config.FIRST_SYMBOL;
+  var _symbol = _config.SYMBOL;
+  var hashtags = input.getValue().split(' ');
+  var error;
+  for (var i = 0; i < hashtags.length; i++) {
+    var hashtag = hashtags[i];
+    if (hashtag[0] !== _symbol) {
+      error = _config.getMessage() + ' «' + hashtag + '»';
+      input.errors.push(error);
+    }
   }
 };
 
-Validation.prototype.isFirstSymbol = function (errors, value, config) {
-  var _symbol = config.SYMBOL;
-  var error = config.getMessage() + '«' + value + '»';
-  if (value[0] !== _symbol) {
-    errors.push(error);
-  }
-};
-
-Validation.prototype.checkQuantity = function (errors, value, config) {
-  var _max = config.MAX;
-  var error = config.getMessage() + ' «' + value + '»';
-  var _value = value.split(' ');
+Validation.prototype.checkQuantity = function (input) {
+  var _config = input.config.QUANTITY;
+  var _max = _config.MAX;
+  var _value = input.getValue().split(' ');
+  var error = _config.getMessage() + ' «' + _value + '»';
   if (_value.length > _max) {
-    errors.push(error);
+    input.errors.push(error);
   }
 };
 
-Validation.prototype.isUnique = function (errors, value, config) {
+Validation.prototype.isUnique = function (input) {
+  var _config = input.config.UNIQUE;
+  var _hashtags = input.getValue().split(' ');
+  var error;
   var uniques = [];
   var isUnique = true;
 
-  value.some(function (hashtag) {
+  _hashtags.some(function (hashtag) {
     var _hashtag = hashtag.toLowerCase();
     if (uniques.includes(_hashtag)) {
-      error = config.getMessage() + hashtag;
+      error = _config.getMessage() + hashtag;
       isUnique = false;
     } else {
       uniques.push(hashtag);
     }
   });
   if (!isUnique) {
-    errors.push(error);
+    input.errors.push(error);
   }
 };
 
-Validation.prototype.checkSymbols = function (errors, value, config ) {
-  var error = config.getMessage() + ' «' + value + '»';
-  var _value;
-  if (value[0] === '#') {
-    _value = value.split(' ').slice(1).join('');
-  } else {
-    _value = value;
-  }
-  if (!config.REGEXP.test(_value)) {
-    errors.push(error);
+Validation.prototype.checkSymbols = function (input) {
+  var _config = input.config.ALLOWED_SYMBOLS;
+  var error;
+  var _hashtags = input.getValue().split(' ');
+  var _firstSymbol = _config.FIRST_SYMBOL_EXEPTION;
+  for (var i = 0; i < _hashtags.length; i++) {
+    var hashtag = _hashtags[i];
+    if (hashtag[0] === _firstSymbol) {
+      hashtag = hashtag.split('').slice(1).join('');
+      if (hashtag === '') {
+        error = _config.getMessage(true);
+        input.errors.push(error);
+      } else {
+        if (_config.REGEXP.test(hashtag)) {
+          error = _config.getMessage(false) + hashtag;
+          input.errors.push(error);
+        }
+      }
+    }
   }
 };
 
@@ -704,16 +783,17 @@ Validation.prototype.close = function () {
 };
 
 Validation.prototype.showErrors = function (inputs) {
+  this.inputsContainer = this.form.querySelector('.img-upload__text');
+
   for (var i = 0; i < inputs.length; i++) {
     var input = inputs[i];
     if (!input.errors.length) {
       input.element.setCustomValidity('');
     } else {
-      input.element.setCustomValidity(input.getErrors())
+      input.element.setCustomValidity(input.getErrors());
       this.invalidities = this.invalidities.concat(input.errors);
     }
   }
-  this.inputsContainer = this.form.querySelector('.img-upload__text');
   var list;
   if (this.isValid) {
     list = document.createElement('ul');
@@ -741,4 +821,3 @@ Validation.prototype.getErrors = function (errors) {
 };
 
 var form = new Form();
-
